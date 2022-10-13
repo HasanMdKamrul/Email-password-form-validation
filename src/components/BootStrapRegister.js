c65bb8777ecc0199683cfac4e8be82b5f26c5fde
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-13 20:29:05 +0200

    Password strength authentication

--- src/components/BootStrapRegister.js
+++ src/components/BootStrapRegister.js
@@ -1,4 +1,5 @@
 import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import app from '../firebase/firebase.init';
@@ -7,12 +8,31 @@ const auth = getAuth(app)
 
 function BootStrapRegister() {
 
+  const [passwordError, setPasswordError] = useState("");
+
   const handleSubmit = event => {
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
     
-    console.log(email,password)
+    console.log(email,password);
+
+    if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+      setPasswordError('Please provide at least 2 uppercase letters');
+      return;
+    };
+
+    if (password.length < 6) {
+      setPasswordError('Please provide at least 6 charecters');
+      return;
+    };
+
+    if (!/(?=.*[!@#$&*])/.test(password)) {
+      setPasswordError('Ensure string has one special case letter.');
+      return;
+    }
+
+    setPasswordError('');
 
    const emailAndPassAuthentication = async () =>{
     try {
@@ -34,7 +54,7 @@ function BootStrapRegister() {
      <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" name="email" placeholder="Enter email" />
+        <Form.Control type="email" name="email" placeholder="Enter email" required />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -42,11 +62,12 @@ function BootStrapRegister() {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control name="password" type="password" placeholder="Password" />
+        <Form.Control name="password" type="password" placeholder="Password" required/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
+      <p>{passwordError}</p>
       <Button variant="primary" type="submit">
         Submit
       </Button>
